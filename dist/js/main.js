17ffe69d7a4772dfbb2d949d87c928b81fb408fd
samhayder <sams.seul@gmail.com>
2020-06-21 00:27:56 +0600

    Project with isotope

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,4 +16,13 @@ $(document).ready(function () {
       $('.navbar').removeClass('navbar-background');
     }
   });
+  // init Isotope
+  let $grid = $('.grid').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on('click', 'button', function () {
+    let filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
 });
